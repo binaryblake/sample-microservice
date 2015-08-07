6d6ddbd406f27104fc8e8f38d8eff827b5285b91
Blake Janelle <blake.janelle2@gmail.com>
2015-08-07 11:15:37 -0400

    Update SampleController.java

--- src/main/java/com/lmig/ci/lmb/devops/SampleController.java
+++ src/main/java/com/lmig/ci/lmb/devops/SampleController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String hello(){
-		return "hello world!"; 
+		return "hello john!"; 
 	}
 }
